252830e1a66aaeb3cadebc89217f2532d604708c
Mayaz47 <74346839+Mayaz47@users.noreply.github.com>
2021-07-10 00:47:58 +0600

    Created HTML files

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeScreen() {
+    return (
+        <div>
+           <h1>Welcome to 11<sup>th</sup> Hour Assistance</h1> 
+        </div>
+    )
+}
+
+export default HomeScreen
