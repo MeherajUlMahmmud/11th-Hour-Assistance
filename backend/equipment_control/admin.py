8a9e47615a8a30d4d6a59080fd470c05eee1a818
Ahsan <73601218+Tonmoy-Ahsan@users.noreply.github.com>
2021-08-09 21:48:11 +0600

    view configuration completed

--- backend/equipment_control/admin.py
+++ backend/equipment_control/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from equipment_control.models import *
 
-
 admin.site.register(EquipmentRequestModel)
+admin.site.register(EquipmentPostModel)
--- backend/equipment_control/urls.py
+++ backend/equipment_control/urls.py
@@ -3,9 +3,15 @@ from django.urls import path
 from equipment_control.views import *
 
 urlpatterns = [
-    path('post-equipment-request/', createEquipmentRequest),
-    path('equipment-requests/', getEquipmentRequests),
-    path('equipment-request/<str:id>', getEquipmentRequestById),
-    path('update-equipment-request/<str:id>', updateEquipmentRequest),
-    path('delete-equipment-request/<str:id>', deleteEquipmentRequest)
+    path('create-new-request/', createEquipmentRequest),
+    path('all-requests/', getEquipmentRequests),
+    path('request/<str:pk>/', getEquipmentRequestById),
+    path('edit-request/<str:pk>/', updateEquipmentRequest),
+    path('delete-request/<str:pk>/', deleteEquipmentRequest),
+    path('create-new-post/', createPost),
+    path('all-post/', getAllPosts),
+    path('post/<str:pk>/', getSinglePost),
+    path('edit-post/<str:pk>/', editPost),
+    path('delete-post/<str:pk>/', deletePost),
+
 ]
