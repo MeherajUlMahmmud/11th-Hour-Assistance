a5d353f607d73bf9e807b5f0e273b43ded7e3c12
Meheraj <meherajmahmmd@gmail.com>
2021-08-21 22:18:54 +0600

    testing request control urls completed

---
+++ backend/request_control/tests/test_urls.py
@@ -0,0 +1,26 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from request_control.views import *
+
+
+class TestRequestControlUrls(SimpleTestCase):
+
+    def test_create_request(self):
+        url = reverse("create-request")
+        self.assertEquals(resolve(url).func, createRequest)
+
+    def test_get_all_request(self):
+        url = reverse('get-all-requests')
+        self.assertEquals(resolve(url).func, getAllRequests)
+
+    def test_get_single_request(self):
+        url = reverse('request-details', args=['1'])
+        self.assertEquals(resolve(url).func, getSingleRequest)
+
+    def test_update_request(self):
+        url = reverse('update-request', args=['1'])
+        self.assertEquals(resolve(url).func, updateRequest)
+
+    def test_delete_request(self):
+        url = reverse('delete-request', args=['1'])
+        self.assertEquals(resolve(url).func, deleteRequest)
--- backend/request_control/urls.py
+++ backend/request_control/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from request_control.views import *
 
 urlpatterns = [
-    path('create-new-request/', createRequest),
-    path('all-requests/', getAllRequests),
-    path('request/<str:pk>/', getSingleRequest),
-    path('update-request/<str:pk>/', updateRequest),
-    path('delete-request/<str:pk>/', deleteRequest),
+    path('create-new-request/', createRequest, name='create-request'),
+    path('all-requests/', getAllRequests, name="get-all-requests"),
+    path('request/<str:pk>/', getSingleRequest, name="request-details"),
+    path('update-request/<str:pk>/', updateRequest, name="update-request"),
+    path('delete-request/<str:pk>/', deleteRequest, name="delete-request"),
 ]
