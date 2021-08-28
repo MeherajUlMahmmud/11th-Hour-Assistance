0238b0849f53e38aac25b3cdeec1d9bc2f746657
Meheraj <meherajmahmmd@gmail.com>
2021-08-28 21:55:07 +0600

    created models

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user_control.models import User
 
-# Create your models here.
+
+class ArticleModel(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
