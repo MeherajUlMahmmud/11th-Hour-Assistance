df99a95eab313f15cc82c0296f0c7e76794bbd2c
Meheraj <meherajmahmmd@gmail.com>
2021-09-11 13:14:54 +0600

    updated article actions for article update

--- frontend/src/actions/articleActions.js
+++ frontend/src/actions/articleActions.js
@@ -42,3 +42,45 @@ export const createArticle = () => async (dispatch, getState) => {
       });
     }
   };
+
+export const updateArticle = (article) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ARTICLE_UPDATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/articles/update-article/${article.id}/`,
+      article,
+      config
+    );
+    dispatch({
+      type: ARTICLE_UPDATE_SUCCESS,
+      payload: data,
+    });
+
+    dispatch({
+      type: ARTICLE_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ARTICLE_UPDATE_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
