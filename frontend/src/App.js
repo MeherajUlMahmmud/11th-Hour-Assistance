0486543c1cb6a8119e1ed69ca4385cc194de8ac1
Meheraj <meherajmahmmd@gmail.com>
2022-01-08 12:12:34 +0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,45 +16,57 @@ import ArticleHomeScreen from "./screens/article/ArticleHomeScreen";
 import ArticlePostScreen from "./screens/article/ArticlePostScreen";
 import ArticleDetailsScreen from "./screens/article/ArticleDetailsScreen";
 import ArticleUpdateScreen from "./screens/article/ArticleUpdateScreen";
-import ProfileScreen from "./screens/ProfileScreen";
+import ProfileScreen from "./screens/user/ProfileScreen";
 import ProfileUpdateScreen from "./screens/user/ProfileUpdateScreen";
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <main>
-        <Route path="/" component={HomeScreen} exact />
-        <Route path="/blood-requests" component={BloodRequestScreen} />
-        <Route path="/request/:id" component={RequestDetailsScreen} />
-        <Route path="/create-new-request" component={PostRequestScreen} />
-        <Route path="/update-request/:id" component={UpdateRequestScreen} />
-        <Route path="/equipment-requests" component={EquipmentRequestScreen} />
-        <Route
-          path="/equipment-request/:id"
-          component={EquipmentRequestDetailsScreen}
-        />
-        <Route
-          path="/create-new-equipment-request"
-          component={PostEquipmentRequestScreen}
-        />
-        <Route
-          path="/update-equipment-request/:id"
-          component={UpdateEquipmentRequestScreen}
-        />
-        <Route path="/articles" component={ArticleHomeScreen} />
-        <Route path="/article/:id" component={ArticleDetailsScreen} />
-        <Route path="/create-article" component={ArticlePostScreen} />
-        <Route path="/edit-article/:id" component={ArticleUpdateScreen} />
+    return (
+        <Router>
+            <Header />
+            <main>
+                <Route path="/" component={HomeScreen} exact />
+                <Route path="/blood-requests" component={BloodRequestScreen} />
+                <Route path="/request/:id" component={RequestDetailsScreen} />
+                <Route
+                    path="/create-new-request"
+                    component={PostRequestScreen}
+                />
+                <Route
+                    path="/update-request/:id"
+                    component={UpdateRequestScreen}
+                />
+                <Route
+                    path="/equipment-requests"
+                    component={EquipmentRequestScreen}
+                />
+                <Route
+                    path="/equipment-request/:id"
+                    component={EquipmentRequestDetailsScreen}
+                />
+                <Route
+                    path="/create-new-equipment-request"
+                    component={PostEquipmentRequestScreen}
+                />
+                <Route
+                    path="/update-equipment-request/:id"
+                    component={UpdateEquipmentRequestScreen}
+                />
+                <Route path="/articles" component={ArticleHomeScreen} />
+                <Route path="/article/:id" component={ArticleDetailsScreen} />
+                <Route path="/create-article" component={ArticlePostScreen} />
+                <Route
+                    path="/edit-article/:id"
+                    component={ArticleUpdateScreen}
+                />
 
-        <Route path="/login" component={LoginScreen} />
-        <Route path="/register" component={RegisterScreen} />
-        <Route path="/profile" component={ProfileScreen} />
-        <Route path="/update-profile" component={ProfileUpdateScreen} />
-      </main>
-      <Footer />
-    </Router>
-  );
+                <Route path="/login" component={LoginScreen} />
+                <Route path="/register" component={RegisterScreen} />
+                <Route path="/profile" component={ProfileScreen} />
+                <Route path="/update-profile" component={ProfileUpdateScreen} />
+            </main>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
